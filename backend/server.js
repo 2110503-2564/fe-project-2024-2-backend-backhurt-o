4dc885a29b4569e8e7d035d37ebf480459272f92
Nicesaktawat <nicesaktawat@gmail.com>
2025-03-24 23:21:18 +0700

    Add persistent authentication state

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ require('dotenv').config({ path: './config/config.env' });
 // Connect to MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
